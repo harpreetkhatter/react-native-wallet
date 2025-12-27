a6a4f57f10e609ce40929ea8bb0e85c70aecfdec
unknown <khatterharpreet@gmail.com>
2025-12-27 13:56:35 +0530

    backend

---
+++ backend/src/config/db.js
@@ -0,0 +1,23 @@
+import {neon} from '@neondatabase/serverless';
+import "dotenv/config"
+
+export const sql = neon(process.env.DATBASE_URL);
+
+export async function initDB() {
+    try {
+        await sql`CREATE TABLE  IF NOT EXISTS transactions(
+            id SERIAL PRIMARY KEY,
+            user_id VARCHAR(255) NOT NULL,
+            title VARCHAR(255) NOT NULL,
+            amount DECIMAL(10,2) NOT NULL,
+            category VARCHAR(100) NOT NULL,
+            created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        ) `
+
+        console.log("Database initialized successfully");
+    } catch (error) {
+        console.error("Error creating table:", error);
+        process.exit(1) //failure
+
+    }
+}
---
+++ backend/src/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv, { parse } from "dotenv"
+import { initDB, sql } from './config/db.js';
+import rateLimiter from './middleware/rateLimiter.js';
+import transactionsRoute from './routes/transactionsRoute.js';
+
+dotenv.config()
+
+const app = express()
+app.use(express.json());
+app.use(rateLimiter)
+
+const port = process.env.PORT;
+
+
+app.use("/api/transactions", transactionsRoute);
+
+initDB().then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+});
